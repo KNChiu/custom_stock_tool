6f16e06218c6abb4eaf2f78bf74a841c3e92d89b
KNChiu <y9760210@gmail.com>
2022-01-28 10:38:49 +0800

    爬取股價並以 CSV 存取

--- model.py
+++ model.py
@@ -2,31 +2,41 @@ import yfinance as yf
 import pandas as pd
 from pandas_datareader import data
 from datetime import datetime
+import os
 
 class StockTool():
 
     def __init__(self, 
-        target_stock = ['0050.tw'], 
-        date_range = "20200101-20210101"
+        target_stock = '0050.tw', 
+        date_range = "20200101-20210101",
+        csv_save_path = r"data"
     ):
 
         self.targetStock = target_stock         # 股票代號
         self.dateRange = date_range             # 起訖日期
+        self.CSVSavePath = csv_save_path        # CSV 存放位置
 
-    def crawler2pandas(self):
+    def crawler2pandas(self):                   # 爬取股價並以 pandas 格式存取
         yf.pdr_override()                       # 以 pandasreader格式
 
         date_range = self.dateRange             # 繼承區間
         start_date = datetime(int(date_range[0:4]), int(date_range[5:6]), int(date_range[7:8]))         # 日期解析
         end_date = datetime(int(date_range[9:13]), int(date_range[14:15]), int(date_range[16:17]))
 
-        return data.get_data_yahoo(self.targetStock, start_date, end_date)    # 將資料存入 Dataframe
+        return data.get_data_yahoo([self.targetStock], start_date, end_date)    # 將資料存入 Dataframe
 
-
-    # # filename = f'./data/{target_stock}.csv' #以股票名稱命名檔案，放在data資料夾下面
-    # # df.to_csv(filename) #將df轉成CSV保存
+    def crawler2CSV(self):                      # 爬取股價並以 CSV 存取 
+        df = self.crawler2pandas()
+        filename = os.path.join(str(self.CSVSavePath), self.targetStock)
+        print("save at :", filename + '.csv')
+        df.to_csv(filename + '.csv')            #將df轉成CSV保存
 
 
 if __name__ == '__main__':
+
+    target_stock = ['0050.tw']
+    date_range = "20200101-20210101"
+
     st_tool = StockTool()
     print(st_tool.crawler2pandas())             # 顯示 pandas 
+    # st_tool.crawler2CSV()
