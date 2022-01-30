c48145aa7b633d60380aa8b48e93448f2de8e416
KNChiu <y9760210@gmail.com>
2022-01-30 21:07:41 +0800

    價值平均法(有BUG)

---
+++ value_averaging.py
@@ -0,0 +1,73 @@
+#%%
+from backtesting import Backtest, Strategy      # 引入回測和交易策略功能
+
+class ValueAveraging(Strategy): 
+    mounthCost = 5000       # 定期定額金額
+    dayContcycle = 30       # 定期定額週期
+    
+    
+    def init(self):
+        self.dayCont = 0            # 天數計數
+        self.mounthCont = 0         # 週期計數
+        self.expectedValue = 0      # 預期價值
+        self.actualValue = 0        # 實際價值
+        self.sumShares = 0          # 股數總和
+        self.cashSurplus = 0        # 現金盈餘
+
+        pass
+
+    def next(self):
+        
+        if self.dayCont > self.dayContcycle:    # 當到了定期購買週期
+            self.dayCont = 0
+
+            self.expectedValue = self.mounthCont * self.mounthCost
+            self.actualValue = self.sumShares * self.data.Close[-1]
+
+            print("預期價值 :", self.expectedValue, "實際價值 :", round(self.actualValue))
+
+            if self.actualValue > self.expectedValue :      # 如果實際價值大於預計價值
+                sellSharessize = (self.actualValue - self.expectedValue) // self.data.Close[-1]
+                self.sell(size = sellSharessize)            # 賣出多餘股數
+                self.cashSurplus = sellSharessize * self.data.Close[-1]
+                self.sumShares -= sellSharessize
+
+            elif self.actualValue < self.expectedValue :
+                buySharessize = (self.expectedValue - self.actualValue) // self.data.Close[-1]
+                self.buy(size = buySharessize)              # 買入不足股數
+                self.sumShares += buySharessize
+
+            elif self.expectedValue ==  0:                  # 第一次購入
+                buySharessize = self.mounthCost // self.data.Close[-1]
+                print(buySharessize)
+                self.buy(size = buySharessize)              # 買入第一次股數
+                self.sumShares += buySharessize
+
+            print("股票價值 :", round(self.sumShares * self.data.Close[-1]), "現金盈餘 :", round(self.cashSurplus))
+            print(self.mounthCont)
+
+            self.mounthCont += 1
+
+        else:
+            self.dayCont += 1
+
+if __name__ == '__main__':
+    from trading_tool.model import StockTool
+
+    target_stock='0050.tw'
+    date_range = "20200101-20220128"
+
+    st_tool = StockTool(target_stock='0050.tw', date_range = "20200101-20211223")
+    df = st_tool.crawler2pandas()
+    # st_tool.crawler2CSV()
+
+    test = Backtest(df, ValueAveraging, cash=75000, commission=.004)
+    # 指定回測程式為test，在Backtest函數中依序放入(資料來源、策略、現金、手續費)
+
+    result = test.run()
+    #執行回測程式並存到result中
+
+    print(result) # 直接print文字結果
+    print(result['_trades'])
+
+    test.plot(filename=f"./backtest_result/SmaCross_{target_stock}_{date_range}.html") #將線圖網頁依照指定檔名保存
